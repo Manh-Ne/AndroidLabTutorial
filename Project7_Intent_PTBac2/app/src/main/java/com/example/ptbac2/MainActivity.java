e0100f7a482cf0fad0b652ff6a2fcfb813e0ae4a
Manh-Ne <manhtlu19@gmail.com>
2024-09-20 19:10:07 +0700

    Project7 Intent PTBac2

---
+++ Project7_Intent_PTBac2/app/src/main/java/com/example/ptbac2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ptbac2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edta, edtb;
+    Button btnkq;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edta =  findViewById(R.id.txta);
+        edtb = findViewById(R.id.txtb);
+        btnkq = findViewById(R.id.btnketqua);
+        btnkq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, ResultActivity.class);
+                Bundle bundle1 = new Bundle();
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                bundle1.putInt("So A", a);
+                bundle1.putInt("So B", b);
+                myintent.putExtra("mybackage", bundle1);
+                startActivity(myintent);
+            }
+        });
+    }
+}
